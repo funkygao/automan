8711f148265e7720971f6702ff972f92ae6cadba
funkygao <funky.gao@gmail.com>
2014-03-14 12:17:52 +0800

    example of usage InputTag

---
+++ example.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Service;
+
+class example {
+
+    /**
+     * @InputTag()
+     * @param array $param
+     * @return array
+     */
+    public function helloWorld($param) {
+        return array(
+            'uid' => 5,
+            'status' => 'ok',
+        );
+    }
+}
