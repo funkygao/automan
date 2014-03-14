3ee4a5d39c45e095ed0b5a698c77a548a235b227
funkygao <funky.gao@gmail.com>
2014-03-14 14:41:08 +0800

    use calss for abstraction

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,3 @@ require_once '/Users/gaopeng/fun/dragon-server-code/v2/init.php';
 require_once 'annotation/annotations.php';
 require_once 'tag/InputTag.php';
 require_once 'tag/OutputTag.php';
-
-define('HTTP_HOST', 'http://localhost');
