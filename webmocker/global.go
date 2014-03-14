9b55c8333def730fd9af11e72e751812d4d8c473
funkygao <funky.gao@gmail.com>
2014-03-14 23:43:50 +0800

    404 error

--- webmocker/global.go
+++ webmocker/global.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	conf "github.com/funkygao/jsconf"
 )
 
@@ -8,10 +9,13 @@ const (
 	LISTEN_ADDR = ":9001"
 	DEBUG_ADDR  = ":9002"
 	CONFIG      = "automan.json"
+	VERSION     = "0.1.2.stable"
 )
 
 var (
 	cf      *conf.Conf
 	apis    map[string]apiConfig // key is uri path
 	verbose bool
+
+	ErrNotFound = errors.New("uri not found")
 )
