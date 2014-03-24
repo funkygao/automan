acb296bd8d75cdffac414171768652d1e43986b8
funkygao <funky.gao@gmail.com>
2014-03-24 18:12:27 +0800

    keep json format in confluence

--- feed_confluence.py
+++ feed_confluence.py
@@ -3,22 +3,50 @@
 '''Prerequirement:
 sudo easy_install confluence '''
 
-import os
+import sys
+import json
 from confluence import Confluence
 
 URL='http://wiki.localhost.cn/'
-USER='test'
-PASS='test'
+USER='peng.gao'
+PASS='peng.gao'
 SPACE='DG'
+
+def wikitag(line):
+    js = json.loads(line)
+    return '{code:JavaScript}' + json.dumps(js, indent=4) + '{code}'
+
+def wiki_content(fn):
+    n = 0
+    content = 'The following content is genereated by automan\n\n'
+    for line in open(fn):
+        if n == 0:
+            # title
+            content += line
+        elif n == 1:
+            # input json
+            content += 'Request:\n'
+            content += wikitag(line) + '\n'
+        elif n == 2:
+            # ouptut json
+            content += 'Response:\n'
+            content += wikitag(line) + '\n'
+        n += 1
+        n %= 3
+
+    return content
 
 def main():
     cf = Confluence(url=URL, username=USER, password=PASS)
-    ok = cf.storePageContent('test auto wiki', SPACE, 'hello world, blah')
+    ok = cf.storePageContent('test auto wiki', SPACE, wiki_content(sys.argv[1]))
     if ok:
-        print 'feed ok'
+        print 'feed wiki ok'
     else:
-        print 'feed fail'
-
+        print 'feed wiki fail'
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print 'Usage: %s api_filename' % sys.argv[0]
+        sys.exit(0)
+
     main()
