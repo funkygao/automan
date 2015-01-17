162e41596f868c523a516a3274273a69445cf94d
funkygao <funky.gao@gmail.com>
2015-01-17 17:42:51 +0800

    validate tag

--- tag/JsonTag.php
+++ tag/JsonTag.php
@@ -11,13 +11,17 @@ class JsonTag extends Annotation
      */
     public function getJsonArray($filename = '')
     {
-        if ($this->value[0] == '@') {
+        if ($this->value && $this->value[0] == '@') {
             $jsonFile = dirname($filename) . '/'
                 . substr($this->value, -(strlen($this->value)-1));
             $this->value = file_get_contents($jsonFile);
         }
 
         $arr = json_decode($this->value, TRUE);
+        if (json_last_error() != JSON_ERROR_NONE) {
+            echo "Invalid tag defined: \n";
+            print_r($this);
+        }
         if (is_array($arr)) {
             return $arr;
         } else {
